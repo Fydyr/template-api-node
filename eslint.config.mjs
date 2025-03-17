01b8300f30f88a62709569945742a07e2209f4e7
Enzo <fournierenzo62@gmail.com>
2025-03-17 15:52:50 +0100

    chore: init project

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            'no-debugger': 'error',
+        },
+    },
+    {
+        ignores: ['node_modules', 'build'],
+    },
+];
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { usersRouter } from './users/users.router';
+export const app = express();
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+
+const port = process.env.PORT || 3000;
+app.use(express.json());
+
+const swaggerDocument = YAML.load(
+    path.join(__dirname, './common/swagger.yaml'),
+);
+
+// Serveur Swagger UI à l'adresse /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(usersRouter);
+
+export const server = app.listen(port);
+
+export function stopServer() {
+    server.close();
+}
