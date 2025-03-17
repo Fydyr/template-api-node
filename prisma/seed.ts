376bfa672a89c547e0211f9aa6cf70d984906283
Enzo <fournierenzo62@gmail.com>
2025-03-17 15:52:50 +0100

    chore: init project

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
+
+const prisma = new PrismaClient();
+
+async function main() {
+    // Suppression de tous les posts
+    await prisma.type.deleteMany();
+    await prisma.pokemonCard.deleteMany();
+
+    const hash_admin = bcrypt.hashSync('admin', saltRounds);
+
+    await prisma.user.createMany({
+        data: {
+            email: 'admin@mail.com',
+            password: hash_admin,
+        },
+    });
+
+    console.log('🌱 Seed completed!');
+}
+
+main()
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
---
+++ src/users/users.router.ts
@@ -0,0 +1,16 @@
+import {
+    createUser,
+    connectionUser,
+    getUsers,
+    deleteUser,
+    patchUser,
+} from './users.controller';
+import { Router } from 'express';
+
+export const usersRouter = Router();
+
+usersRouter.get('/users', getUsers);
+usersRouter.post('/user', createUser);
+usersRouter.post('/user/login', connectionUser);
+usersRouter.patch('/user/:userId', patchUser);
+usersRouter.delete('/user/:userId', deleteUser);
